11a7c60a9cb017bee3c6660d44629cd5b1a8966a
sonda2208 <ace.solix@gmail.com>
2019-03-27 14:30:26 +0700

    Implement parse from JSON string

--- parser.go
+++ parser.go
@@ -1,1 +1,171 @@
 package rules
+
+import (
+	"encoding/json"
+	"errors"
+	"reflect"
+)
+
+type CompType string
+
+const (
+	OrCond  CompType = "or"
+	AndCond CompType = "and"
+)
+
+type OpType string
+
+const (
+	Eq OpType = "eq"
+)
+
+type Rule struct {
+	Comparator CompType    `json:"comparator,omitempty"`
+	Rules      []Rule      `json:"rules,omitempty"`
+	Var        string      `json:"var,omitempty"`
+	Op         OpType      `json:"op,omitempty"`
+	Val        interface{} `json:"val,omitempty"`
+}
+
+func (r *Rule) IsValid() error {
+	if len(r.Comparator) > 0 {
+		if len(r.Rules) == 0 {
+			return errors.New(`"Rules" must not be empty in complex mode`)
+		}
+
+		if len(r.Var) > 0 {
+			return errors.New(`"Var" must be empty in complex mode`)
+		}
+
+		if len(r.Op) > 0 {
+			return errors.New(`"Op" must be empty in complex mode`)
+		}
+
+		if r.Val != nil {
+			return errors.New(`"Val" must be nil in complex mode`)
+		}
+	} else {
+		if len(r.Var) == 0 {
+			return errors.New(`"Var" must not be empty`)
+		}
+
+		if len(r.Op) == 0 {
+			return errors.New(`"Op" must not be empty`)
+		}
+
+		if r.Val == nil {
+			return errors.New(`"Val" must not be nil`)
+		}
+	}
+
+	return nil
+}
+
+func ParseFromJSON(raw json.RawMessage) (Expr, error) {
+	rule := Rule{}
+	err := json.Unmarshal(raw, &rule)
+	if err != nil {
+		return nil, err
+	}
+
+	expr, err := parseExpr(&rule)
+	if err != nil {
+		return nil, err
+	}
+
+	return expr, nil
+}
+
+func parseExpr(rule *Rule) (Expr, error) {
+	err := rule.IsValid()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(rule.Rules) > 0 {
+		var op Op
+		switch rule.Comparator {
+		case OrCond:
+			op = OR
+		case AndCond:
+			op = AND
+		}
+
+		var res Expr
+		for _, r := range rule.Rules {
+			e, err := parseExpr(&r)
+			if err != nil {
+				return nil, err
+			}
+
+			if res == nil {
+				res = e
+			} else {
+				res = &BinaryExpr{
+					Op:  op,
+					LHS: res,
+					RHS: e,
+				}
+			}
+		}
+
+		return &ExprExpr{
+			Expr: res,
+		}, nil
+	}
+
+	switch rule.Op {
+	case Eq:
+		typeof := reflect.TypeOf(rule.Val)
+		if typeof == nil {
+			return nil, errors.New("invlaid type")
+		}
+
+		switch typeof.Kind() {
+		case reflect.Int:
+			return &BinaryExpr{
+				Op:  EQ,
+				LHS: &Var{rule.Var},
+				RHS: &NumberLiteral{Val: float64(rule.Val.(int))},
+			}, nil
+		case reflect.Int32:
+			return &BinaryExpr{
+				Op:  EQ,
+				LHS: &Var{rule.Var},
+				RHS: &NumberLiteral{Val: float64(rule.Val.(int32))},
+			}, nil
+		case reflect.Int64:
+			return &BinaryExpr{
+				Op:  EQ,
+				LHS: &Var{rule.Var},
+				RHS: &NumberLiteral{Val: float64(rule.Val.(int64))},
+			}, nil
+		case reflect.Float32:
+			return &BinaryExpr{
+				Op:  EQ,
+				LHS: &Var{rule.Var},
+				RHS: &NumberLiteral{Val: float64(rule.Val.(float32))},
+			}, nil
+		case reflect.Float64:
+			return &BinaryExpr{
+				Op:  EQ,
+				LHS: &Var{rule.Var},
+				RHS: &NumberLiteral{Val: float64(rule.Val.(float64))},
+			}, nil
+		case reflect.String:
+			return &BinaryExpr{
+				Op:  EQ,
+				LHS: &Var{rule.Var},
+				RHS: &StringLiteral{Val: rule.Val.(string)},
+			}, nil
+		case reflect.Bool:
+			return &BinaryExpr{
+				Op:  EQ,
+				LHS: &Var{rule.Var},
+				RHS: &BoolLiteral{Val: rule.Val.(bool)},
+			}, nil
+		}
+	}
+
+	return nil, errors.New("unexpected error")
+}
