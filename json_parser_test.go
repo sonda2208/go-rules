1a2c739b9d613d73fb37db84884d47e0b21cde9c
sonda2208 <sonda2208@gmail.com>
2019-10-18 12:44:00 +0700

    Rename old JSON parser, handle custom function and add tests

--- json_parser_test.go
+++ json_parser_test.go
@@ -9,11 +9,55 @@ import (
 	rules "github.com/sonda2208/go-rules"
 )
 
-func TestParser(t *testing.T) {
-	tests := []struct {
+func TestJSONParser(t *testing.T) {
+	testcases := []struct {
 		rules   string
 		isError bool
 	}{
+		{
+			`{ "var": "strlen(a)", "op": ">", "val": 1 }`,
+			false,
+		},
+		{
+			`{ "var": "a", "op": "==", "val": false }`,
+			false,
+		},
+		{
+			`{ "var": "a", "op": "==", "val": 0 }`,
+			false,
+		},
+		{
+			`{ "var": "a", "op": "==", "val": -1 }`,
+			false,
+		},
+		{
+			`{ "var": "a", "op": "==", "val": 1 }`,
+			false,
+		},
+		{
+			`{ "var": "a", "op": "==", "val": "0" }`,
+			false,
+		},
+		{
+			`{ "var": "a", "op": "==", "val": "2h" }`,
+			false,
+		},
+		{
+			`{ "var": "a", "op": "==", "val": "2006-01-02T15:04:05Z" }`,
+			false,
+		},
+		{
+			`{ "var": "a", "op": "in", "val": [1, 2, 3] }`,
+			false,
+		},
+		{
+			`{ "var": "a", "op": "in", "val": ["1", "2", "3"] }`,
+			false,
+		},
+		{
+			`{ "comparator": "&&", "rules": [ { "comparator": "||", "rules": [ { "var": "a", "op": "==", "val": "xavier" }, { "var": "b", "op": "==", "val": 1 } ] }, { "var": "c", "op": "in", "val": [2, 3, 4] } ] }`,
+			false,
+		},
 		{
 			`{ "comparator": "&&", "rules": [ { "comparator": "||", "rules": [ { "var": "a", "op": "==", "val": "xavier" }, { "var": "b", "op": "==", "val": 1 } ] }, { "var": "c", "op": "==", "val": true } ] }`,
 			false,
@@ -39,30 +83,62 @@ func TestParser(t *testing.T) {
 			true,
 		},
 		{
+			`{ "var": "a", "op": "=", "val": 1 }`,
+			true,
+		},
+		{
 			`{ "var": "a", "op": "in", "val": [] }`,
 			true,
 		},
 		{
-			`{ "var": "a", "op": "in", "val": [1, 2, 3] }`,
-			false,
+			`{ "var": "a", "op": "in", "val": [0, "1", 2] }`,
+			true,
 		},
 		{
-			`{ "var": "a", "op": "in", "val": ["1", "2", "3"] }`,
-			false,
+			`{ "var": "a", "op": "in", "val": [true, false] }`,
+			true,
 		},
 		{
-			`{ "comparator": "&&", "rules": [ { "comparator": "||", "rules": [ { "var": "a", "op": "==", "val": "xavier" }, { "var": "b", "op": "==", "val": 1 } ] }, { "var": "c", "op": "in", "val": [2, 3, 4] } ] }`,
-			false,
+			`{ "var": "a", "op": "in", "val": [0, 1, false] }`,
+			true,
+		},
+		{
+			`{ "var": "a", "op": "", "val": "2h" }`,
+			true,
+		},
+		{
+			`{ "var": "", "op": "==", "val": "2h" }`,
+			true,
+		},
+		{
+			`{ "var": "a", "op": "=="}`,
+			true,
+		},
+		{
+			`{"comparator": "||" }`,
+			true,
+		},
+		{
+			`{ "comparator": "||", "rules": [] }`,
+			true,
+		},
+		{
+			`{ "comparator": "&&", "rules": [ { "comparator": "||", "rules": [ { "var": "", "op": "==", "val": "xavier" }, { "var": "b", "op": "==", "val": 1 } ] }, { "var": "c", "op": "in", "val": [2, 3, 4] } ] }`,
+			true,
+		},
+		{
+			`{ "var": "strlen(a", "op": ">", "val": 1 }`,
+			true,
 		},
 	}
 
-	for _, test := range tests {
-		expr, err := rules.ParseFromJSON([]byte(test.rules))
-		if test.isError {
-			assert.Error(t, err)
+	for _, tc := range testcases {
+		expr, err := rules.ParseFromJSON([]byte(tc.rules))
+		if tc.isError {
+			assert.Error(t, err, tc.rules)
 		} else {
 			require.NoError(t, err)
-			assert.NotNil(t, expr)
+			assert.NotNil(t, expr, tc.rules)
 		}
 	}
 }
